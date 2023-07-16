e83bba6f2213bd8f1e5ea33a123ac1808a9a489d
Andro <ranogaet@gmail.com>
2023-07-16 20:29:36 +0300

    boyOrGirl

---
+++ boyOrGirl/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine() ?? string.Empty;
+string uniqualizedString = new string(input.Distinct().ToArray());
+if(uniqualizedString.Length % 2 != 0)
+{
+    Console.WriteLine("IGNORE HIM!");
+}
+else
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
---
+++ helpful maths/hm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    
+    int n;
+    std::cin >> n;
+    std::string *s = new std::string[n];
+
+
+
+
+    return 0;
+}
