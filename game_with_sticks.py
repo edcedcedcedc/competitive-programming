e5b692e29cff463166807ff2c3b109a243385037
edcedcedcedc <ranogaet@gmail.com>
2025-04-30 13:18:48 +0300

    problems

---
+++ game_with_sticks.py
@@ -0,0 +1,15 @@
+import math
+
+n, m = list(map(int, input().split()))
+if math.floor((n + m) / 2) % 2 == 0:
+    print("Malvika")
+else:
+    print("Akshat")
+
+
+""" 
+
+
+the answer to this is wrong, I cannot understand the logic yet ..
+
+ """
